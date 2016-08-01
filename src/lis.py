3e04e4ab8168691fcfa69b43874da3a50cdc8f6e
sptzxbbb <sptzxbbb@gmail.com>
2016-08-01 13:37:36 +0800

    Finish eval()

--- src/lis.py
+++ src/lis.py
@@ -22,22 +22,33 @@ def standard_env():
     env = Env()
     env.update(vars(math))
     env.update({
-        '+':op.add, '-':op.sub, '*',op.mul, '/',op.div,
+        '+':op.add, '-':op.sub, '*':op.mul, '/':op.truediv,
         '>':op.gt, '<':op.lt, '>=':op.ge, '<=': op.le, '=':op.eq,
         'abs': abs,
         'append': op.add,
-        'apply': apply,
         'begin': lambda *x: x[-1],
         'car': lambda x: x[0],
         'cdr': lambda x: x[1:],
-        'cons': lambda x,y: [x] + y,
+        'cons': lambda x, y: [x] + y,
         'eq?': op.is_,
         'equal?': op.eq,
         'length': len,
-        'list': lambda *x: list[x]
-        'list?' lambda x: isinstance(x, list)
-
+        'list': lambda *x: list(x),
+        'list?': lambda x: isinstance(x, list),
+        'map': map,
+        'max': max,
+        'min': min,
+        'not': op.not_,
+        'null?': lambda x: x == [],
+        'number?': lambda x: isinstance(x, Number),
+        'procedure?': callable,
+        'round': round,
+        'symbol?': lambda x: isinstance(x, Symbol),
     })
+    return env
+
+global_env = standard_env()
+
 
 def tokenize(chars):
     """
@@ -84,10 +95,33 @@ def parse(program):
     return read_from_tokens(tokenize(program))
 
 
+def eval(x, env=global_env):
+    """
+    Evaluate an expression in an environment.
+    """
+    if isinstance(x, Symbol):
+        return env[x]
+    elif not isinstance(x, List):
+        return x
+    elif x[0] == 'if':
+        (_, test, conseq, alt) = x
+        exp = (conseq if eval(test, env) else alt)
+        return eval(exp, env)
+    elif x[0] == 'define':
+        (_, var, exp) = x
+        env[var] = eval(exp, env)
+    else:
+        proc = eval(x[0], env)
+        args = [eval(arg, env) for arg in x[1:]]
+        return proc(*args)
+
+
 def main():
     "Only for test"
-    program = "(begin (define r 10) (* pi (* r r)))"
-    print(parse(program))
+    program = "(define r 10)"
+    ans = eval(parse(program))
+    ans = eval(parse("(* pi (* r r))"))
+    print(ans)
 
 if __name__ == '__main__':
     main()
